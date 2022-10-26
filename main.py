b8329e47845b1f0d576d1d83365075af9232e5dc
Yong <yong.park123456@gmail.com>
2022-10-25 18:52:16 -0600

    puntos que se mueven

--- main.py
+++ main.py
@@ -11,18 +11,71 @@ screen = pygame.display.set_mode(
 
 x = 10
 y = 10
+x1 = 600
+y1 = 500
 
-glClearColor(0.0,1.0,0.0,1.0)
+def pixel(x,y,color):
+    glEnable(GL_SCISSOR_TEST)
+    glScissor(x,y,100,100)
+    glClearColor(color[0],color[1],color[2],1.0)
+    glClear(GL_COLOR_BUFFER_BIT)
+    glDisable(GL_SCISSOR_TEST)
 
+
+speedx = 1
+speedy = 1
+speedx1 = 1
+speedy1 = 1
 running = True
 while running:
     #clean
+    glClearColor(0.1,0.8,0.2,1.0)
     glClear(GL_COLOR_BUFFER_BIT)
 
     #paint
-    # screen.set_at((x,y),(255,255,255))
-    # x += 1
-    # y += 1
+    pixel(x,y,(1.0,0.0,0.0))
+    x += speedx
+    y += speedy
+
+    if x == 800:
+        speedx = -1
+    if x == 0:
+        speedx = 1
+    if y == 600:
+        speedy = -1
+    if y == 0:
+        speedy = 1
+
+    pixel(x1,y1,(1.0,1.0,0.0))
+    x1 += speedx1
+    y1 += speedy1
+
+    if x1 == 800:
+        speedx1 = -1
+    if x1 == 0:
+        speedx1 = 1
+    if y1 == 600:
+        speedy1 = -1
+    if y1 == 0:
+        speedy1 = 1
+
+    if x == x1:
+        if x > 400:
+            speedx = -1
+            speedx1 = 1
+        else:
+            speedx = 1
+            speedx1 = -1
+
+    if y == y1:
+        if y > 300:
+            speedy = -1
+            speedy1 = 1
+        else:
+            speedy = 1
+            speedy1 = -1
+
+  
 
     #flip 
     pygame.display.flip()
