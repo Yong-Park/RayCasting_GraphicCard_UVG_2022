06b1906ae77eabd539a5b62e3bc688dc27ff799f
Yong <yong.park123456@gmail.com>
2022-10-25 18:24:15 -0600

    pantalla pintada

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import pygame
 
+from OpenGL.GL import *
+
 pygame.init()
 
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode(
+    (800,600),
+    pygame.OPENGL | pygame.DOUBLEBUF
+)
 
 x = 10
 y = 10
 
+glClearColor(0.0,1.0,0.0,1.0)
+
 running = True
 while running:
     #clean
-
+    glClear(GL_COLOR_BUFFER_BIT)
 
     #paint
-    screen.set_at((x,y),(255,255,255))
-    x += 1
-    y += 1
+    # screen.set_at((x,y),(255,255,255))
+    # x += 1
+    # y += 1
 
     #flip 
     pygame.display.flip()
